c8df8e250cbbf1e34751e8149371ba6fbbf9496f
SalakDF <tarasgalako@gmail.com>
2026-05-15 19:42:36 +0300

    Update project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,8 @@ import TeamsPage from "./pages/TeamsPage";
 import TeamDetailsPage from "./pages/TeamDetailsPage";
 import StandingsPage from "./pages/StandingsPage";
 import NotFoundPage from "./pages/NotFoundPage";
+import FavoritesPage from "./pages/FavoritesPage";
+import AuthPage from "./pages/AuthPage";
 
 export default function AppRouter() {
   return (
@@ -18,6 +20,8 @@ export default function AppRouter() {
         <Route path="teams" element={<TeamsPage />} />
         <Route path="teams/:id" element={<TeamDetailsPage />} />
         <Route path="standings" element={<StandingsPage />} />
+        <Route path="favorites" element={<FavoritesPage />} />
+        <Route path="auth" element={<AuthPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
